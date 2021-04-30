f64f3dc38657344083f08a92d5826dcc851de59d
Huang Tianhua <huangtianhua@HuangdeMacBook-Pro.local>
2021-04-30 16:50:53 +0800

    update google

--- huang.tianhua/demo/js/maps.js
+++ huang.tianhua/demo/js/maps.js
@@ -6,17 +6,18 @@ const makeMap = async (target,center={lat: 37.735078,lng: -122.436844}) => {
 
    // map = map_el.data("map");
 
-   if(!map_el.data("map")) map_el.data(
-      "map",
-      new google.maps.Map(
+   if(!map_el.data("map")) map_el.data({
+      "map": new google.maps.Map(
          map_el[0],
          {
             center: center,
             zoom: 12,
-            disableDefaultUI: true
+            disableDefaultUI: true,
+            styles:mapStyles
          }
-      )
-   );
+      ),
+      "infoWindow": new google.maps.InfoWindow({content:''})
+   });
 
    return map_el;
 }
@@ -45,4 +46,158 @@ const makeMarkers = (map_el,map_locs) => {
    });
 
    map_el.data("markers",markers);
+   setTimeout(()=>{setMapBounds(map_el,map_locs)},150);
 }
+
+
+const setMapBounds = (map_el,map_locs) => {
+   let map = map_el.data("map");
+   let zoom = 14;
+
+   if(map_locs.length==1) {
+      map.setCenter(map_locs[0]);
+      map.setZoom(zoom);
+   } else if(map_locs.length==0) {
+      if(window.location.protocol!=="https:") return;
+      else {
+         navigator.geolocation.getCurrentPosition(p=>{
+            let pos = {
+               lat:p.coords.latitude,
+               lat:p.coords.longitude
+            };
+            map.setCenter(pos);
+            map.setZoom(zoom);
+         },(...args)=>{
+            console.log(args)
+         },{
+            enableHighAccuracy:false,
+            timeout:5000,
+            maximumAge:0
+         })
+      }
+   } else {
+      let bounds = new google.maps.LatLngBounds(null);
+      map_locs.forEach(o=>{
+         bounds.extend(o);
+      });
+      map.fitBounds(bounds);
+   }
+}
+
+
+
+
+let mapStyles = [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#c0e4f3"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+];
+
+// let mapStyles = [
+//     {
+//         "stylers": [
+//             {
+//                 "hue": "#2c3e50"
+//             },
+//             {
+//                 "saturation": 250
+//             }
+//         ]
+//     },
+//     {
+//         "featureType": "road",
+//         "elementType": "geometry",
+//         "stylers": [
+//             {
+//                 "lightness": 50
+//             },
+//             {
+//                 "visibility": "simplified"
+//             }
+//         ]
+//     },
+//     {
+//         "featureType": "road",
+//         "elementType": "labels",
+//         "stylers": [
+//             {
+//                 "visibility": "off"
+//             }
+//         ]
+//     }
+// ];
